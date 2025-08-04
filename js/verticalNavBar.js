17a6156c606893c4fd1bcfca441bff33b825d301
coreyhsGames <83317368+coreyhsGames@users.noreply.github.com>
2025-08-05 09:44:45 +1200

    vertical navigation bar fix

--- js/verticalNavBar.js
+++ js/verticalNavBar.js
@@ -5,8 +5,12 @@ const closeBtn = document.getElementById('close-btn');
 
 hamburger.addEventListener('click', function () {
     verticalNavBar.style.width = "100%";
+    hamburger.style.display = "none";
+    closeBtn.style.display = "block";
 });
 
 closeBtn.addEventListener('click', function () {
     verticalNavBar.style.width = "0%";
+    hamburger.style.display = "block";
+    closeBtn.style.display = "none";
 });
