05fdfbba8b367fa84f5e427be20c8d8b08ad2bee
coreyhsGames <83317368+coreyhsGames@users.noreply.github.com>
2025-10-20 23:34:02 +1300

    removed 'logo' as it was never used

--- public/js/shrinkNavBar.js
+++ public/js/shrinkNavBar.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navBar = document.querySelector('.nav-bar-horizontal');
-    const logo = document.querySelector('.logo');
     
     // Set the scroll threshold for when the navbar should shrink
     const scrollThreshold = 300;
